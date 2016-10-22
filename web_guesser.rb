f069a13d073468400230c6f6f7480e5eaa2973fc
Arsirion <arsirion@gmail.com>
2016-10-22 17:54:31 -0500

    finished

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 num = rand(100) 
-
+# guess = params['guess'].to_i
 get '/' do
-	erb :index, :locals => {:num => num}
+	guess = params['guess'].to_i
+	guess_checker(guess, num)
+		
+	
+end
+
+def guess_checker(guess, num)
+		
+	if (guess < 0 || guess > 100)
+		error = "Please enter a valid number"
+	elsif guess > num 
+		error = "Guess is too high"
+	elsif guess < num
+		error = "Guess is too low"
+	else 
+		error = "Nailed it"
+	end
+	erb :index, :locals => {:num => num, :error => error}	
 end
